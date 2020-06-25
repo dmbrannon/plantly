39ae24a07d238528f1c6f218d18f662b3a3a58f2
Dana <dana.brannon@utexas.edu>
2020-06-25 15:47:06 -0500

    Incorporate templates and include Bootstrap

--- plantly/journal/views.py
+++ plantly/journal/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+plants = [
+    {
+        'name':'Bird of Paradise',
+        'location':'Kitchen',
+        'last_watered':'11/01/2020',
+        'last_fertilized':'11/3/2020',
+        'bought':'10/31/2020',
+        'notes':'Leaf yellowed'
+    },
+    {
+        'name':'Snake Plant',
+        'location':'Bedroom',
+        'last_watered':'12/01/2020',
+        'last_fertilized':'12/3/2020',
+        'bought':'11/29/2020',
+        'notes':'Repotted'
+    },
+]
 
 def home(request):
-    return HttpResponse('<h1>Journal Home</h1>')
+    context = {
+        'plants': plants
+    }
+    return render(request, 'journal/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Journal About</h1>')
+    return render(request, 'journal/about.html', {'title':'About'})
