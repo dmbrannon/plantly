5a05696573f17f4a1d9390704716d84dbcfee648
Dana <dana.brannon@utexas.edu>
2020-07-07 12:22:20 -0500

    Add password email reset functionality

--- plantly/settings.py
+++ plantly/settings.py
@@ -129,3 +129,10 @@ MEDIA_URL =  '/media/' #how to access media through browser
 
 LOGIN_REDIRECT_URL = 'journal-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
