ccd723fd1f558c93849200a3bd427a6814272c90
dmbrannon <dana.brannon@utexas.edu>
2020-07-14 20:00:57 +0000

    Add non-www domain to localhosts

--- plantly/settings.py
+++ plantly/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS1'), os.environ.get('ALLOWED_HOSTS2')]
 
 
 # Application definition
