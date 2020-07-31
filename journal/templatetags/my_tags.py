4df9cc97c53c9d69761df0afcb38eb964fb3601c
Dana <dana.brannon@utexas.edu>
2020-07-31 15:03:09 -0500

    Change time_since to days ago, add more info modal on mobile

--- journal/templatetags/my_tags.py
+++ journal/templatetags/my_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from django.template.defaultfilters import stringfilter
+from datetime import date, datetime
 
 register = template.Library()
 
@@ -8,3 +9,28 @@ register = template.Library()
 def upto(value, delimiter=None):
     return value.split(delimiter)[0]
 upto.is_safe = True
+
+@register.filter(expects_localtime=True)
+def days_since(value, arg=None):
+    try:
+        tzinfo = getattr(value, 'tzinfo', None)
+        value = date(value.year, value.month, value.day)
+    except AttributeError:
+        # Passed value wasn't a date object
+        return value
+    except ValueError:
+        # Date arguments out of range
+        return value
+    today = datetime.now(tzinfo).date()
+    delta = value - today
+    if abs(delta.days) == 1:
+        day_str = ("day")
+    else:
+        day_str = ("days")
+
+    if delta.days < 1:
+        fa_str = ("ago")
+    else:
+        fa_str = ("from now")
+
+    return "%s %s %s" % (abs(delta.days), day_str, fa_str)
